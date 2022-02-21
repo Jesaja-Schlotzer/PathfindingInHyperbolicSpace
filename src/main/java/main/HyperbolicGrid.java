d58f08c00d3ce8e0ac4b70b43bac04ca37db1f54
Jesaja Schlotzer <jesaja@schlotzer.de>
2022-02-21 11:58:45 +0100

    Initial commit

---
+++ src/main/java/main/HyperbolicGrid.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HyperbolicGrid {
+
+    private Map<String, Integer> grid = new HashMap<>();
+
+
+    public HyperbolicGrid() {
+
+    }
+}
